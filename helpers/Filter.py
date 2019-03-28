4ad3517c620fa6e06ad1c1b33b9da72eb1c7a06c
Marko Štumberger <marko.stumberger@gmail.com>
2019-03-28 15:42:03 +0100

    fixed filter to search name and surname if they start with prefix

--- helpers/Filter.py
+++ helpers/Filter.py
@@ -1,10 +1,10 @@
 from helpers.Contact import Contact
 
+
 def filter_by_prefix(prefix: str, persons_list: list):
     prefix = prefix.lower()
-    return list(filter(lambda person: ((prefix in person.name.lower())|
-                                       (prefix in person.surname.lower())|
-                                       (prefix in person.phone.lower())), persons_list))
+    return list(filter(lambda person: ((person.name.lower().startswith(prefix)) |
+                                       (person.surname.lower().startswith(prefix))), persons_list))
 
 
 if __name__ == "__main__":
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     assert len(filter_by_prefix("kli", test_persons_list)) == 1
     assert len(filter_by_prefix("kl", test_persons_list)) == 2
 
-    for filtered_person in filter_by_prefix("Kl", test_persons_list):
+    for filtered_person in filter_by_prefix("eme", test_persons_list):
         print(filtered_person.to_string())
